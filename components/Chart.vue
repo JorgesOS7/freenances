6584566d4aa81391c8c269de8cf9bf3882c980f9
Jose Jorge <josejorge.b.10@hotmail.com>
2022-01-21 15:41:17 -0300

    Viewer de chart

---
+++ components/Chart.vue
@@ -0,0 +1,77 @@
+<template>
+  <apexchart type="bar" :options="chartOptions" :series="series"></apexchart>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartOptions: {
+        colors: ["#7e22ce", "#e11d48"],
+        chart: {
+          width: "100%",
+          height: 350,
+          zoom: {
+            enabled: false,
+          },
+          toolbar: {
+            show: false,
+          },
+        },
+        plotOptions: {
+          bar: {
+            distributed: true,
+          },
+        },
+        tooltip: {
+          enabled: false,
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "straight",
+        },
+        legend: {
+          show: false,
+        },
+        grid: {
+          show: true,
+          xaxis: {
+            lines: {
+              show: false,
+            },
+          },
+          yaxis: {
+            lines: {
+              show: false,
+            },
+          },
+        },
+        xaxis: {
+          labels: {
+            show: false,
+          },
+        },
+        yaxis: {
+          labels: {
+            show: false,
+          },
+        },
+      },
+    };
+  },
+  computed: {
+    series() {
+      return [
+        {
+          data: [
+            Math.floor(Math.random() * 10000),
+            Math.floor(Math.random() * 10000),
+          ],
+        },
+      ];
+    },
+  },
+};
+</script>
--- pages/charts.vue
+++ pages/charts.vue
@@ -1,14 +1,28 @@
 <template>
-  <main class="flex-1 overflow-y-scroll">
+  <main class="flex-1 overflow-y-auto">
     <header class="bg-purple-700 border-b">
       <div class="flex justify-between items-center container mx-auto p-6">
         <h1 class="text-xl text-white">Resumo por categoria</h1>
-        <button class="p-1.5">
+        <button class="p-1.5 text-white">
           <ph-export :size="24" />
         </button>
       </div>
     </header>
 
-    <section class="container mx-auto px-6 pb-20"></section>
+    <section class="container mx-auto px-6 pb-20">
+      <div class="max-w-xl mx-auto mt-5">
+        <chart />
+        <div class="flex items-center justify-center gap-x-20">
+          <div>
+            <h1 class="font-semibold text-sm text-gray-600">Entradas</h1>
+            <p class="text-xl text-purple-600">R$ 14.400,00</p>
+          </div>
+          <div>
+            <h1 class="font-semibold text-sm text-gray-600">Saídas</h1>
+            <p class="text-xl text-rose-600">R$ 400,00</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
