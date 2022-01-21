1f823dec85f938a37135dd2077bb2825aa19e97e
Jose Jorge <josejorge.b.10@hotmail.com>
2022-01-21 13:59:22 -0300

    Criação dos componentes tab e tabitem

---
+++ components/Tab.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer
+    class="
+      block
+      lg:hidden
+      fixed
+      bottom-0
+      inset-x-0
+      bg-white
+      border-t
+      rounded-t-3xl
+    "
+  >
+    <nav class="max-w-lg mx-auto">
+      <ul class="flex items-center justify-between">
+        <tab-item to="/" icon="ph-house" />
+        <tab-item to="/transactions" icon="ph-arrows-left-right" />
+        <tab-item to="/charts" icon="ph-chart-pie-slice" />
+      </ul>
+    </nav>
+  </footer>
+</template>
---
+++ components/TabItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li class="flex-1 grid place-items-center">
+    <nuxt-link
+      class="p-3 flex flex-col items-center justify-center relative"
+      :to="to"
+      v-slot="{ navigate, isExactActive }"
+      custom
+    >
+      <button @click="navigate" :class="[isExactActive && 'dot']">
+        <component
+          :is="icon"
+          :size="32"
+          :weight="isExactActive ? 'fill' : 'regular'"
+          :class="{
+            'text-indigo-600': isExactActive,
+            'text-gray-400': !isExactActive,
+          }"
+        />
+      </button>
+    </nuxt-link>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    to: String,
+    icon: String,
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.dot {
+  @apply after:bottom-1
+        after:absolute
+        after:w-1.5
+        after:h-1.5
+        after:bg-indigo-600
+        after:rounded-full;
+}
+</style>
